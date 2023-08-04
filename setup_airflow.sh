9d71ac200c277afca1a5a3c2d280b34031efbb2a
dredge67 <david.reimertz@gmail.com>
2023-08-04 02:51:27 +0000

    edits

---
+++ setup_airflow.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export AIRFLOW_HOME="/workspaces/hands-on-introduction-data-engineering-4395021/airflow"
+
+export AIRFLOW_VERSION=2.5.1
+PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+# echo "Installing Airflow Version $AIRFLOW_VERSION, with Python $PYTHON_VERSION"
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+# pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+
+env | grep -i airflow
