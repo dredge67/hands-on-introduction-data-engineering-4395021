34d0a255ba2cb2bd80899c3a133cd6b84ab3866e
dredge67 <david.reimertz@gmail.com>
2023-08-04 14:24:08 +0000

    Add script and fetched data

---
+++ lab/manual/fetchdata.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget \
+    -c https://datahub.io/core/top-level-domain-names/r/top-level-domain-names.csv.csv \
+    -O /workspaces/hands-on-introduction-data-engineering-4395021/lab/manual/manual-extract-data.csv
+
